68acacd04b1b1c3fa0ec556b09246b2b6c9db97b
ashu8826 <ashutosh16012@iiitd.ac.in>
2016-08-16 19:49:38 +0530

    Rejected commit: Default

---
+++ app/src/main/java/com/mc/ashu16012/quiz_assignfirst/QuizActivity.java
@@ -0,0 +1,13 @@
+package com.mc.ashu16012.quiz_assignfirst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
